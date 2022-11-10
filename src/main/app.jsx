a48f0cf2d16c2e03953eb2e309891cc556375999
Pedro <pedro7109.j@gmail.com>
2022-11-10 08:24:08 -0300

    Component PageHeader

--- src/main/app.jsx
+++ src/main/app.jsx
@@ -1,15 +1,13 @@
-import 'module/bootstrap/dist/css/bootstrap.min.css'
-import 'module/font-awesome/css/font-awesome.min.css'
+import 'modules/bootstrap/dist/css/bootstrap.min.css'
+import 'modules/font-awesome/css/font-awesome.min.css'
 
 import React from 'react'
-import Todo from '../todo/todo'
-import About from '../about/about'
 import Menu from '../templete/menu'
+import Routes from './Routes'
 
 export default props => (
     <div className='container'>
         <Menu/>
-        <Todo/>
-        <About/>
+       <Routes/>
     </div>
 )
