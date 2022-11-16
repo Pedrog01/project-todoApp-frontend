2b01dc5bb87bd2ca56667810df5326c2749d3783
Pedro <pedro7109.j@gmail.com>
2022-11-16 07:59:17 -0300

    add event in handleAdd

--- src/todo/Todo.jsx
+++ src/todo/Todo.jsx
@@ -1,7 +1,11 @@
 import React,  {Component} from "react";
+import axios from "axios";
+
 import PageHeader from "../templete/pageHeader";
 import TodoForm from "./TodoForm";
 import TodoList from "./TodoList";
+
+const URL = 'http://localhost:3003/api/todos';
 
 export default class Todo extends Component {
 
@@ -14,7 +18,8 @@ export default class Todo extends Component {
     }
 
     handleAdd(){
-        console.log(this.state.description)
+        const description = this.state.description
+        axios.post(URL,{ description }).then(resp => console.log('Funcionou !'))
     }
 
     handleChange(e){
